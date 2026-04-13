516c86c1a43bf3ca32fa28cebb378ef129bb7458
TrigunDev <triguntamragouri@gmail.com>
2026-04-13 13:09:56 +0530

    Update WishlistRepository.java

--- src/main/java/com/skillbarter/repository/WishlistRepository.java
+++ src/main/java/com/skillbarter/repository/WishlistRepository.java
@@ -1,5 +1,4 @@
 package com.skillbarter.repository;
-
 import com.skillbarter.entity.WishlistItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
