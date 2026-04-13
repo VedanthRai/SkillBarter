4daa1dddf85730fcd49763fb7d6131128028ecf6
TrigunDev <triguntamragouri@gmail.com>
2026-04-13 11:52:16 +0530

    Updated

--- src/main/java/com/skillbarter/enums/UserStatus.java
+++ src/main/java/com/skillbarter/enums/UserStatus.java
@@ -1,9 +1,6 @@
 package com.skillbarter.enums;
 
-/**
- * User account lifecycle states.
- * Used in the User Account State Machine (State Diagram 3).
- */
+
 public enum UserStatus {
     PENDING_VERIFICATION,   // Registered, email not confirmed
     ACTIVE,                 // Normal operating state
